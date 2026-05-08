9c2063dfabcfcfb00573999ae47452f5ab6efa97
hahalee001-uniforce <111046060+hahalee001-uniforce@users.noreply.github.com>
2026-05-08 12:45:01 +0800

    Update i_cndr_you.sh

--- i_cndr_you.sh
+++ i_cndr_you.sh
@@ -6,7 +6,7 @@ while true
 do
 	echo "I WILL MOUNT HOST FS!" >> $LOG_NAME
 	mkdir -p /data/demo
-	mount /dev/sda2 /data/demo
+	mount /boot /data/demo
 
     echo "BWAHAAA MOUNTED HOST FS!" >> $LOG_NAME
 	#every 9 hours
