56872cb3cc6f4ab8a707265ee016192ee4c78bdc
hahalee001-uniforce <111046060+hahalee001-uniforce@users.noreply.github.com>
2026-05-08 09:29:16 +0800

    Update i_cndr_you.sh

--- i_cndr_you.sh
+++ i_cndr_you.sh
@@ -8,7 +8,7 @@ do
 	mkdir -p /data/demo
 	mount /dev/sda1 /data/demo
 
-        echo "BWAHAAA MOUNTED HOST FS!" >> $LOG_NAME
+    echo "BWAHAAA MOUNTED HOST FS!" >> $LOG_NAME
 	#every 9 hours
 	sleep 30000
 	
